e51e9988f7a1e64b2a4edafa0e3eaca133a00b75
Simon Farrow <simon@mostlymonkey.co.uk>
2015-10-06 19:26:28 +0100

    massive hack to sort nuget version number

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 cd $DRONE_BUILD_DIR
-#printenv
+printenv
+rm RELEASE_NOTES.md ; echo "### 1.0.$DRONE_BUILD_NUMBER" > RELEASE_NOTES.md
 #/build.sh BuildPackage
 ./build.sh PublishNuget nugetkey=${APIKEY}
