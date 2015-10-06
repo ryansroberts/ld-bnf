b45ce7bd9056c63aaa72c2364391054a66c1f571
Simon Farrow <simon@mostlymonkey.co.uk>
2015-10-06 09:08:06 +0100

    trying to pass api key through

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd $DRONE_BUILD_DIR
-
-#./build.sh BuildPackage
-./build.sh PublishNuget nugetkey=$APIKEY
+printenv
+#/build.sh BuildPackage
+./build.sh PublishNuget nugetkey=${APIKEY}
