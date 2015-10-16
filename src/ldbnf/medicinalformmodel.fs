984c1fed4c3483b695cf7ade4d6042cf5e1b88b9
Simon Farrow <simon@mostlymonkey.co.uk>
2015-10-16 11:10:56 +0100

    expand model

--- src/ldbnf/medicinalformmodel.fs
+++ src/ldbnf/medicinalformmodel.fs
@@ -16,23 +16,21 @@ module MedicinalForm =
 
   type Manufacturer = | Manufacturer of string
 
-  type MedicinalProductTitle = | MedicinalProductTitle of Manufacturer * mfProvider.Title
+  type MedicinalProductTitle = | MedicinalProductTitle of Option<Manufacturer> * mfProvider.Title
 
-  type Ampid = | Ampid of int
+  type Ampid = | Ampid of int64
 
   type StrengthOfActiveIngredient = | StrengthOfActiveIngredient of mfProvider.P
 
   //To be extended in the future
   type UnitOfMeasure =
     | Tablet
-    | Dunno
 
   type PackSize = | PackSize of decimal
 
   //I'm pretty sure that there will be more of these
   type LegalCategory =
     | POM
-    | Dunno
 
   type PackInfo = | PackInfo of Option<PackSize> * Option<UnitOfMeasure> * Option<LegalCategory>
 
@@ -43,18 +41,18 @@ module MedicinalForm =
 
   type NhsIndicativePrice = | NhsIndicativePrice of decimal
 
-  type NhsIndicativeInfo = | NhsIndicativeInfo of NhsIndicative * PriceText * NhsIndicativePrice
+  type NhsIndicativeInfo = | NhsIndicativeInfo of Option<NhsIndicative> * Option<PriceText> * Option<NhsIndicativePrice>
 
 
   type DrugTarrif = | DrugTarrif of string
 
-  type DrugTariffPrice = | DrugTariffPrice of decimal
+  type DrugTariffPrice = | DrugTariffPrice of string
 
-  type DrugTariffInfo = | DrugTariffInfo of DrugTarrif * PriceText * DrugTariffPrice
+  type DrugTariffInfo = | DrugTariffInfo of Option<DrugTarrif> * Option<PriceText> * Option<DrugTariffPrice>
 
 
 
-  type Pack = | Pack of PackInfo * NhsIndicativeInfo * DrugTariffInfo
+  type Pack = | Pack of Option<PackInfo> * Option<NhsIndicativeInfo> * Option<DrugTariffInfo>
 
   type MedicinalProduct = {
     title:MedicinalProductTitle;
@@ -71,10 +69,18 @@ module MedicinalFormParser =
   open prelude
   open MedicinalForm
 
+
   let inline withoc n x =
     let oc = (^a : (member Outputclass : string) x)
     if (oc = n) then Some (x)
     else None
+
+  let inline withoco n x =
+    let oc = (^a : (member Outputclass : Option<string>) x)
+    match oc with
+      | Some s ->
+        if (s = n) then Some (x) else None
+      | _ -> None
 
   type CautionaryAdvisoryLabel with
     static member from (x:mfProvider.P) =
@@ -87,32 +93,67 @@ module MedicinalFormParser =
       let t = x.Title.Value >>= (CautionaryAndAdvisoryLabelsTitle >> Some)
       CautionaryAdvisoryLabels(t,ls)
 
-  type PackSize with
-    static member from (x:mfProvider.Ph) = 
-      x.Number >>= (PackSize >> Some)
+  let fromphn c (x:mfProvider.Ph) = 
+    x.Number >>= (c >> Some)
+
+  let fromphs c (x:mfProvider.Ph) = 
+    x.String >>= (c >> Some)
 
   type UnitOfMeasure with
     static member from (x:mfProvider.Ph) =
       match x.String with
         | Some(s) -> match s with
-                     | "tablet" -> Tablet
-                     | _ -> Dunno
-        | None -> Dunno
+                     | "tablet" -> Some(Tablet)
+                     | _ -> None
+        | None -> None
 
   type LegalCategory with
     static member from (x:mfProvider.Ph) =
       match x.String with
         | Some(s) -> match s with
-                     | "POM" -> POM
-                     | _ -> Dunno
-        | None -> Dunno
+                     | "POM" -> Some(POM)
+                     | _ -> None
+        | None -> None
 
   type PackInfo with
     static member from (x:mfProvider.P) =
-      let ps = x.Phs |> Array.tryPick (withoc "packSize") >>= PackSize.from
+      let ps = x.Phs |> Array.tryPick (withoc "packSize") >>= (fromphn PackSize)
       let uom = x.Phs |> Array.tryPick (withoc "unitOfMeasure") >>= UnitOfMeasure.from
       let lc = x.Phs |> Array.tryPick (withoc "legalCategory") >>= LegalCategory.from
       PackInfo(ps,uom,lc)
+ 
+  type NhsIndicativeInfo with
+    static member from (x:mfProvider.P) =
+      let nhsi = x.Phs |> Array.tryPick (withoc "nhsIndicative") >>= (fromphs NhsIndicative)
+      let pt = x.Phs |> Array.tryPick (withoc "priceText") >>= (fromphs PriceText)
+      let nhsip = x.Phs |> Array.tryPick (withoc "nhsIndicativePrice") >>= (fromphn NhsIndicativePrice)
+      NhsIndicativeInfo(nhsi,pt,nhsip)
+
+  type DrugTariffInfo with
+    static member from (x:mfProvider.P) =
+      let dt = x.Phs |> Array.tryPick (withoc "drugTariff") >>= (fromphs DrugTarrif)
+      let pt = x.Phs |> Array.tryPick (withoc "priceText") >>= (fromphs PriceText)
+      let dtp = x.Phs |> Array.tryPick (withoc "drugTariffPrice") >>= (fromphs DrugTariffPrice)
+      DrugTariffInfo(dt,pt,dtp)
+
+  type MedicinalProductTitle with
+    static member from (x:mfProvider.Title) =
+      let m = x.Ph >>= (fromphs Manufacturer)
+      MedicinalProductTitle(m,x)
+
+  type Ampid with
+    static member from (x:mfProvider.Data) =
+      Ampid(x.Value)
+
+  type Pack with
+    static member from (x:mfProvider.Li) =
+      let pi = x.Ps |> Array.tryPick (withoco "packInfo") >>= (PackInfo.from >> Some)
+      let nio = x.Ps |> Array.tryPick (withoco "nhsIndicativeInfo") >>= (NhsIndicativeInfo.from >> Some)
+      let dti = x.Ps |> Array.tryPick (withoco "drugTariffInfo") >>= (DrugTariffInfo.from >> Some)
+      Pack(pi,nio,dti)
+
+  //type MedicinalProduct with
+  //  static member from (x:mfProvider.Section) =
 
   let parse (x:mfProvider.Topic) =
     let t = match x.Title.Value with
