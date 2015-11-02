28fd34b8b5d42274d0ccf4d0066fe4aa91932bd0
Matt Smith <mattonfoot@gmail.com>
2015-11-02 14:09:31 +0000

    correct folders

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #mono splitter/bin/Release/splitter.exe bnf.xml process drug
-mono bin/ldbnf/ldbnf.exe --xmldirectory process --outputdirectory process
+mono bin/ldbnf/ldbnf.exe --xmldirectory process/xml --outputdirectory process/ttl
