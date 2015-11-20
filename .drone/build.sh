c5c3735321e79b760f69be8e05faca7a53f14621
matt <mattonfoot@gmail.com>
2015-11-20 13:08:15 +0000

    attempt to inject secret api key

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+APIKEY=$1
 cd $DRONE_BUILD_DIR
 printenv
 rm RELEASE_NOTES.md ; echo "### 1.0.$DRONE_BUILD_NUMBER" > RELEASE_NOTES.md
 #/build.sh BuildPackage
-./build.sh PublishNuget nugetkey=$$APIKEY
+./build.sh PublishNuget nugetkey${APIKEY}
