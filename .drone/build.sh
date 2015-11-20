75b75a0658d36f9aeaf770ad1c58c8b71520c07e
matt <mattonfoot@gmail.com>
2015-11-20 13:35:38 +0000

    nuget package bump due to new build server

--- .drone/build.sh
+++ .drone/build.sh
@@ -3,6 +3,6 @@ set -e
 APIKEY=$1
 cd $DRONE_BUILD_DIR
 printenv
-rm RELEASE_NOTES.md ; echo "### 1.0.$DRONE_BUILD_NUMBER" > RELEASE_NOTES.md
+rm RELEASE_NOTES.md ; echo "### 1.1.$DRONE_BUILD_NUMBER" > RELEASE_NOTES.md
 #/build.sh BuildPackage
 ./build.sh PublishNuget nugetkey=${APIKEY}
