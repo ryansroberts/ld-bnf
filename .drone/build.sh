eb4cdb0c339c3c7214522107e4107335ae678451
Matt Smith <mattonfoot@gmail.com>
2015-11-24 14:50:09 +0000

    teamcity build config

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 APIKEY=$1
-cd $DRONE_BUILD_DIR
 printenv
-rm RELEASE_NOTES.md ; echo "### 1.1.$DRONE_BUILD_NUMBER" > RELEASE_NOTES.md
+rm RELEASE_NOTES.md ; echo "### 1.2.$BUILD_NUMBER" > RELEASE_NOTES.md
 #/build.sh BuildPackage
 ./build.sh PublishNuget nugetkey=${APIKEY}
