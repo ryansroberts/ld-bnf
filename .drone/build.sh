72c80141a667b8c61723ed643fe7ae07bdb8469b
matt <mattonfoot@gmail.com>
2015-11-25 09:43:00 +0000

    update build task

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-APIKEY=$1
 printenv
-rm RELEASE_NOTES.md ; echo "### 1.2.$BUILD_NUMBER" > RELEASE_NOTES.md
-#/build.sh BuildPackage
-./build.sh PublishNuget nugetkey=${APIKEY}
+rm RELEASE_NOTES.md ; echo "### 1.2.$2" > RELEASE_NOTES.md
+./build.sh PublishNuget nugetkey=$1
